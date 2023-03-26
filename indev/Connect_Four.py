3e1c473ed8ecff1091e6111adee779249d6541b3
Roy Liu <113428371+ruisnake@users.noreply.github.com>
2023-03-26 15:15:08 +0200

    removed duplicated functions
    removed: drop and fourConnectedCheck

--- indev/Connect_Four.py
+++ indev/Connect_Four.py
@@ -4,71 +4,8 @@ def main():
     board = initBoard()
     drop(board, 1);
 
-
-def drop(board, column, color):
-    nrRows = len(board);
-    if(board[column][0] != None):
-        return -1;
-    for y in range(nrRows): #each row
-        if(board[column][nrRows - 1 - y] != None):
-            board[column][nrRows - y] = color;
-            return nrRows - y;
-
-#In this function we only need to check those 
-def fourConnectedCheck(board, row, column, color):
-    nrRows = len(board) - 1;
-    nrColumns = len(board[0]) - 1;
-    #To the right
-    nrConnected = 1;
-    for x in range(nrColumns - column):
-        if(board[column + x][row] == color):
-            nrConnected += 1;
-            if(nrConnected == 4):
-                return True;
-            else:
-                break;
-        else:
-            break;
-    #To the left
-    nrConnected = 1;
-    for x in range(column):
-        if(board[column - x][row] == color):
-            nrConnected += 1;
-            if(nrConnected == 4):
-                return True;
-            else:
-                break;
-        else:
-            break;
-    #South West
-    nrConnected = 1;
-    z = min(column, row);
-    for x in range(z):
-        if(board[column - x][row - x] == color):
-            nrConnected += 1;
-            if(nrConnected == 4):
-                return True;
-            else:
-                break;
-        else:
-            break;
-    #South East
-    nrConnected = 1;
-    z = min(nrColumns - column, row);
-    for x in range(z):
-        if(board[column + x][row - x] == color):
-            nrConnected += 1;
-            if(nrConnected == 4):
-                return True;
-            else:
-                break;
-        else:
-            break;
-    return False;
-
-
 def twoPlayerGame():
-    board = initializeBoard();
+    board = initBoard();
     while(True):
         try:
             print("Welcome to Connect Four! Press 0 for the player with 0 to begin and 1 for player 1 to begin");
@@ -100,13 +37,3 @@ def twoPlayerGame():
             print(msg);
             break;
         playerOneMove = not playerOneMove;
-
-
-
-
-
-
-
-
-
-
