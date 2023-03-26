a417732fba5665f5e7fbe0eb9bc39ec198271b3a
ruisnake <thesnakerl@gmail.com>
2023-03-26 16:25:15 +0200

    Upload from Git

--- indev/Connect_Four.py
+++ indev/Connect_Four.py
@@ -1,26 +1,25 @@
-import gameboard
+import sys
+from gameboard import *
 
-def main():
-    board = initBoard()
-    drop(board, 1);
+sys.path.append("..")
 
 def twoPlayerGame():
     board = initBoard();
     while(True):
         try:
             print("Welcome to Connect Four! Press 0 for the player with 0 to begin and 1 for player 1 to begin");
-            playerOneMove = int(input) == 0;
+            playerOneMove = int(input()) == 0;
             break;
         except:
             print("Error, please only write the number and nothing else");
 
     while(True):
-        printBoard(board);
+        printGame(board);
         while(True):
             try:
-                print("Choose column(0-6) to drop checker");
-                column = int(input);
-                if(column not in range(6)):
+                print("Choose column(1-7) to drop checker");
+                column = int(input());
+                if(column not in range(1,8)): #The user inputs 1 - 7 but in the calculus row value 0 is row the first row. Row 1.
                     print("Something went wrong, try again");
                 else:
                     break;
@@ -31,9 +30,18 @@ def twoPlayerGame():
             color = 0;
         else:
             color = 1;
-        row = drop(board, column, color);
-        if(fourConnectedCheck(board,row, column, color)):
+        row = drop(board, column-1, color);
+        print("Rowin: " + str(row) + "Column: " + str(column-1));
+        if(fourConnectedCheck(board,row, column-1, color)):
+            printGame(board);
             msg = "Player" + str(color+1) + " has won!";
             print(msg);
             break;
         playerOneMove = not playerOneMove;
+
+def main():
+    print("Hello")
+    twoPlayerGame()
+
+if __name__ == "__main__":
+    main()
