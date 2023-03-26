e7459db6b69a82d21a5c046466ed0b436fa60885
Axel Lindqvist <axel.lindqvist@hotmail.se>
2023-03-26 16:49:13 +0200

    Neural network file

---
+++ indev/NN.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+# Define the input layer with 126 neurons
+input_layer = tf.keras.layers.Input(shape=(18,7));
+
+# Reshape the input layer to have a single channel
+reshaped_input = tf.reshape(input_layer, [-1, 18, 7, 1]);
+
+# Define the convolutional layer with  21 neurons in the second layer. Each neuron being covering one 3x3 patch of the gameboard. 
+conv_layer = tf.keras.layers.Conv2D(filters=21, kernel_size=(9, 3), strides=(1, 3), padding="valid", activation="relu")(reshaped_input);
+
+# Flatten the output of the convolutional layer
+flattened_layer = tf.keras.layers.Flatten()(conv_layer)
+
+# Define the output layer with 7 neurons
+output_layer = tf.keras.layers.Dense(units=7, activation="softmax")(flattened_layer)
+
+# Create the model with the input and output layers
+model = tf.keras.models.Model(inputs=input_layer, outputs=output_layer)
+
+# Compile the model
+model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.CategoricalCrossentropy(), metrics=["accuracy"]);
+
+# Print the model summary
+model.summary();
+
+#TODO
+#Remaking a square into one hot encoding
+
+#Process input data
+
